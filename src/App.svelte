f3264a6b8e1b62fcd438c527359030076fb120ad
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-06-30 14:05:10 +0400

    binding

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,8 @@
   import Inner from './lib/Inner.svelte';
   import Outer from './lib/Outer.svelte';
   import CustomButton from './lib/CustomButton.svelte';
- 
+  import {marked} from 'marked';
+
   let name = 'world';
   let user = { loggedIn: false };
   let user2 = { loggedIn: false };
@@ -27,6 +28,22 @@
 		{ id: 4, name: 'doughnut' },
 		{ id: 5, name: 'egg' }
 	];
+  let title='world';
+  let a = 1;
+	let b = 2;
+  let yes = false;
+
+  let scoops = 1;
+	let flavours = ['Mint choc chip'];
+
+  let value = `Some words are *italic*, some are **bold**`;
+
+  function join(flavours) {
+		if (flavours.length === 1) return flavours[0];
+		return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
+	}
+
+	let menu = ['Cookies and cream', 'Mint choc chip', 'Raspberry ripple'];
 
 	function handleClick() {
 		things = things.slice(1);
@@ -110,6 +127,19 @@
 	};
 
   let m = { x: 0, y: 0 };
+  let questions = [
+		{ id: 1, text: `Where did you go to school?` },
+		{ id: 2, text: `What is your mother's name?` },
+		{ id: 3, text: `What is another personal fact that an attacker could easily find with Google?` }
+	];
+
+	let selected;
+
+	let answer = '';
+
+  function handleSubmitBinding() {
+		alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`);
+	}
 
   function handleMousemove(event: { clientX: number; clientY: number; }) {
     m.x = event.clientX;
@@ -238,6 +268,89 @@
   <h3>Events/DOM event forwarding</h3>
   <CustomButton on:click={handleClickButton} />
 
+  <h3>Bindings/Text inputs</h3>
+  <input bind:value={title} />
+  <p>Hello {title}!<p>
+
+  <h3>Bindings/Numerics inputs</h3>
+  <label>
+    <input type="number" bind:value={a} min="0" max="10" />
+    <input type="range" bind:value={a} min="0" max="10" />
+  </label>
+  
+  <label>
+    <input type="number" bind:value={b} min="0" max="10" />
+    <input type="range" bind:value={b} min="0" max="10" />
+  </label>
+  
+  <p>{a} + {b} = {a + b}</p>
+
+  <h3>Bindings/Checkbox inputs</h3>
+  <label>
+    <input type="checkbox" bind:checked={yes} />
+    Yes! Send me regular email spam
+  </label>
+  
+  {#if yes}
+    <p>Thank you. We will bombard your inbox and sell your personal details.</p>
+  {:else}
+    <p>You must opt-in to continue. If you're not paying, you're the product.</p>
+  {/if}  
+  <button disabled={!yes}> Subscribe </button>
+
+
+  <h3>Bindings/Group inputs</h3>
+  <p>Size</p>
+  <label>
+    <input type="radio" bind:group={scoops} name="scoops" value={1} />
+    One scoop
+  </label>
+  <label>
+    <input type="radio" bind:group={scoops} name="scoops" value={2} />
+    Two scoops
+  </label>
+  <label>
+    <input type="radio" bind:group={scoops} name="scoops" value={3} />
+    Three scoops
+  </label>
+  <p>Flavours</p>
+  {#each menu as flavour}
+    <label>
+      <input type="checkbox" bind:group={flavours} name="flavours" value={flavour} />
+      {flavour}
+    </label>
+  {/each}
+
+  {#if flavours.length === 0}
+    <p>Please select at least one flavour</p>
+  {:else if flavours.length > scoops}
+    <p>Can't order more flavours than scoops!</p>
+  {:else}
+    <p>
+      You ordered {scoops}
+      {scoops === 1 ? 'scoop' : 'scoops'}
+      of {join(flavours)}
+    </p>
+  {/if}
+  
+  <h3>Bindings/Textarea inputs</h3>
+  {@html marked(value)}
+  <textarea bind:value />
+
+  <h3>Bindings/Select bindings</h3>
+  <p>Insecurity questions</p>
+  <form on:submit|preventDefault={handleSubmitBinding}>
+    <select bind:value={selected} on:change={() => (answer = '')}>
+      {#each questions as question}
+        <option value={question}>
+          {question.text}
+        </option>
+      {/each}
+    </select>
+	<input bind:value={answer} />
+	<button disabled={!answer} type="submit"> Submit </button>
+  </form>
+  <p>selected question {selected ? selected.id : '[waiting...]'}</p>
 
   <h1>Pokédex</h1>
   <!-- {#await promise}
@@ -267,6 +380,8 @@
   Une erreur s'est produite : {error.message}
 {/await}
  
+
+
 
 </main>
 
